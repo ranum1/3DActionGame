7dbc01d0158b14413d02fa7ea8c37a7158a109cd
Ranum1 <168388235+ranum1@users.noreply.github.com>
2025-04-08 20:38:57 +0900

    3. Make Item

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type { Ammo, Coin, Grenade, Heart, Weapon };
+
+    public Type type;
+    public int value;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+    }
+}
